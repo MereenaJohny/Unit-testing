c23e7f76d228d46e8a2ef6f4065479b6ce71a0ce
mjohny <65822793+MereenaJohny@users.noreply.github.com>
2021-06-09 12:19:41 -0400

    adding files

---
+++ IsItALeapyear/IsItALeapyear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IsItALeapyear
+{
+   public class Program
+    {
+        public bool IsItALeapYear(int y)
+        {
+            if ((y % 4 == 0) && (y % 100 != 0 || y % 400 == 0))
+            {
+                Console.WriteLine(y + ":leap year");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(y + ": not a leap year");
+                return false;
+            }
+        }
+        static void Main(string[] args)
+      
+        {
+            Console.WriteLine("Leap year?!");
+        }
+    }
+}
+
